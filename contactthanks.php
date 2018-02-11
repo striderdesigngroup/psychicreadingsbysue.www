fea1ccfa4e1f99cb9a719eb3ae625c85939f47e9
Sam Strider <sam@clarity-connect.com>
2018-02-11 12:27:23 -0500

    initializing repo in github

---
+++ contactthanks.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thank You!</title>
+    <link rel="stylesheet" type="text/css" href="layout.css" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700|Roboto:400,400italic,700' rel='stylesheet' type='text/css'>
+    <link rel="SHORTCUT ICON" type="image/vnd.microsoft.icon" href="/image/favicon.ico">
+    <link rel="SHORTCUT ICON" type="image/gif" href="/image/favicon.gif">
+</head>
+
+<body>
+	<div id="container">
+    	<div id="header">
+        	<img src="image/logo.png" />
+        	<ul>
+            	<li><a href="index.html">Home</a></li>&nbsp;&nbsp;|&nbsp;&nbsp;
+                <li><a href="services.html">Services</a></li>&nbsp;&nbsp;|&nbsp;&nbsp;
+                <li><a href="rates.html">Rates</a></li>&nbsp;&nbsp;|&nbsp;&nbsp;
+                <li><a href="faqs.html">FAQs</a></li>&nbsp;&nbsp;|&nbsp;&nbsp;
+                <li><a href="feedback.html">Feedback</a></li>&nbsp;&nbsp;|&nbsp;&nbsp;
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </div>
+        
+        <div id="content">
+            <h1>Thank you for contacting me!</h1>
+            <hr>
+            <p>I will be in touch with you soon.</p>
+    	</div>  
+        
+         <div id="footer">
+            <ul>
+                <li><a href="index.html">Home</a></li>&nbsp;&nbsp;|&nbsp;&nbsp;
+                <li><a href="services.html">Services</a></li>&nbsp;&nbsp;|&nbsp;&nbsp;
+                <li><a href="rates.html">Rates</a></li>&nbsp;&nbsp;|&nbsp;&nbsp;
+                <li><a href="faqs.html">FAQs</a></li>&nbsp;&nbsp;|&nbsp;&nbsp;
+                <li><a href="feedback.html">Feedback</a></li>&nbsp;&nbsp;|&nbsp;&nbsp;
+                <li><a href="contact.html">Contact</a></li>
+           </ul>
+            
+           <p>&copy; 2014 Tarot & Intuitive Readings by Sue. Produced by <a href="http://www.samsnyderdesigns.com" target="_blank">Sam Snyder Designs.</a></p>
+    	</div>
+          
+    </div>
+
+
+
+</body>
+</html>
